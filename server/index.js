a1a5c7e706e362f71c75c551d8d2cc2e9c831845
H-vishwa <130923885+H-vishwa@users.noreply.github.com>
2026-06-09 16:59:04 +0530

    Updated the CORS env

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ const authLimiter = rateLimit({
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   }),
 );
